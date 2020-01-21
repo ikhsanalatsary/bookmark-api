97818825d60a8c367b4e10b2b14f1ff4d3895359
ikhsanalatsary <skull.saders18@gmail.com>
2020-01-21 20:18:53 +0700

    setup heroku purposes

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
 			process.env.MONGOHQ_URL ||
       process.env.MONGOLAB_URI ||
       process.env.MONGODB_URI ||
-			'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/bookmark-dev',
+			'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/api_collections',
 		options: {
 			user: '',
 			pass: ''
